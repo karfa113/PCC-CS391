fe5604b7c813beb35efec7b72f2ece3e1772fff0
karfa113 <karfamonojit3117@gmail.com>
2025-07-21 00:51:10 +0530

    add new file

---
+++ ASSIGNMENT 0/A0P1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+    int *p,n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    p = (int *)malloc(n * sizeof(int));
+
+    if(p == NULL){
+        printf("Can't allocate space right now");
+        return 0;
+    }
+
+    printf("Enter elements into the array: \n");
+    for(int i=0; i<n; i++){
+        scanf("%d", &p[i]);
+    }
+
+    printf("Entered array elements are: \n");
+    for(int i=0; i<n; i++){
+        printf("%d ", p[i]);
+    }
+
+    free(p);
+    return 0;
+}
