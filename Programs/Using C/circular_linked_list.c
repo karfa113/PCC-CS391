3b2fa37c27e1cd45724542f6aef1e29d9b8cdb2c
karfa113 <karfamonojit3117@gmail.com>
2025-10-11 20:06:15 +0530

    circular linked list

--- Programs/Using C/circular_linked_list.c
+++ Programs/Using C/circular_linked_list.c
@@ -9,7 +9,7 @@ struct Node{
 struct Node* head = NULL;
 
 void insert();
-void delete();
+void deleteNode(); 
 void display();
 void search();
 
@@ -18,7 +18,7 @@ int main(void){
     while(1){
         printf("\n\n--- Circular Linked List ---\n");
         printf("1. Insert at any position\n");
-        printf("2. Deleter from a position\n");
+        printf("2. Delete from a position (kth from end)\n");
         printf("3. Display the list\n");
         printf("4. Search an element\n");
         printf("5. Exit\n");
@@ -31,7 +31,7 @@ int main(void){
                 insert();
                 break;
             case 2:
-                delete();
+                deleteNode();
                 break;
             case 3:
                 display();
@@ -43,7 +43,7 @@ int main(void){
                 printf("Exiting program ...\n");
                 exit(0);
             default:
-                printf("Invlid choice!");
+                printf("Invalid choice!");
         }
     }
 
@@ -59,11 +59,14 @@ void insert(){
     
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->data = value;
+    newNode->next = NULL;
+
     if(head == NULL){
         head = newNode;
         newNode->next = head;
         return;
     }
+
     if(pos == 1){
         struct Node* temp = head;
         while(temp->next != head){
@@ -72,17 +75,108 @@ void insert(){
         temp->next = newNode;
         newNode->next = head;
         head = newNode;
-    }else{
-        int i=1;
-        struct Node* temp = head;
-        while(i<pos-1 && temp->next != NULL){
-            temp = temp->next;
-        }
-        newNode->next = temp->next;
-        temp->next = newNode; 
+        return;
     }
+
+    struct Node* temp = head;
+    int i = 1;
+    while(i < pos - 1 && temp->next != head){
+        temp = temp->next;
+        i++;
+    }
+
+    newNode->next = temp->next;
+    temp->next = newNode;
 }
 
-void delete(){
-    
+void deleteNode(){
+    if(head == NULL){
+        printf("List is empty!\n");
+        return;
+    }
+
+    int k;
+    printf("Enter position from end to delete (k): ");
+    scanf("%d", &k);
+
+    int len = 1;
+    struct Node* temp = head;
+    while(temp->next != head){
+        len++;
+        temp = temp->next;
+    }
+
+    if(k > len || k <= 0){
+        printf("Invalid position!\n");
+        return;
+    }
+
+    int pos = len - k + 1; 
+    if(pos == 1){
+        struct Node* last = head;
+        while(last->next != head)
+            last = last->next;
+
+        struct Node* del = head;
+        if(head->next == head){ 
+            head = NULL;
+        }else{
+            head = head->next;
+            last->next = head;
+        }
+        free(del);
+        printf("Deleted node from position %d (from end).\n", k);
+        return;
+    }
+
+    temp = head;
+    for(int i = 1; i < pos - 1; i++)
+        temp = temp->next;
+
+    struct Node* del = temp->next;
+    temp->next = del->next;
+    free(del);
+    printf("Deleted node from position %d (from end).\n", k);
+}
+
+void display(){
+    if(head == NULL){
+        printf("List is empty!\n");
+        return;
+    }
+
+    struct Node* temp = head;
+    printf("Circular Linked List: ");
+    do{
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }while(temp != head);
+    printf("\n");
+}
+
+void search(){
+    if(head == NULL){
+        printf("List is empty!\n");
+        return;
+    }
+
+    int value;
+    printf("Enter element to search: ");
+    scanf("%d", &value);
+
+    struct Node* temp = head;
+    int pos = 1, found = 0;
+
+    do{
+        if(temp->data == value){
+            printf("Element %d found at position %d.\n", value, pos);
+            found = 1;
+            break;
+        }
+        temp = temp->next;
+        pos++;
+    }while(temp != head);
+
+    if(!found)
+        printf("Element %d not found in the list.\n", value);
 }
