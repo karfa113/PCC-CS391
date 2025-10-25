fab01ba0854e25107c26952f06b5aa32db6a78f3
karfa113 <karfamonojit3117@gmail.com>
2025-10-25 22:01:49 +0530

    .

--- Programs/Using C/selection_sort.c
+++ Programs/Using C/selection_sort.c
@@ -2,9 +2,9 @@
 
 void selectionSort(int arr[], int n) {
     int i, j, minIndex, temp;
-    for (i = 0; i < n - 1; i++) {
+    for (i=0; i<n-1; i++) {
         minIndex = i;
-        for (j = i + 1; j < n; j++) {
+        for (j = i+1; j<n; j++) {
             if (arr[j] < arr[minIndex]) {
                 minIndex = j;
             }
