80fb9ba8f9bfc0b2e846071429d9b350259a501b
karfa113 <karfamonojit3117@gmail.com>
2025-11-02 10:26:59 +0530

    Updated

--- Programs/Using C/quick_sort.c
+++ Programs/Using C/quick_sort.c
@@ -12,8 +12,7 @@ int partition(int arr[], int low, int high){
 	
 	for(int j=low; j<=high-1; j++){
 		if(arr[j] < pivot){
-			i++;
-			swap(&arr[i], &arr[j]);
+			swap(&arr[++i], &arr[j]);
 		}
 	}
 	swap(&arr[i+1], &arr[high]);
